44b68c99b0643621c7e023db126a55a1c820127a
Yusuke Kominami <komi@Yusukes-MacBook-Air.local>
2018-04-21 18:08:36 +0900

    4/21

--- 5_assimilation_system/assimilation.py
+++ 5_assimilation_system/assimilation.py
@@ -1,12 +1,74 @@
 import sys
 import numpy as np
+from lorenz96 import Lorenz96
 
 
 class Kalman(object):
     def __init__(self, true_path="save_data/data", noise_path="noise_data/data"):
         self.file_num = 1460
+        self.N = 40
         self.true_path = true_path
         self.noise_path = noise_path
+        self.init_true, self.init_noise = self.load_data(0)
+        self.model = Lorenz96(N_dim=self.N, F=8, init_x=self.init_true)
+
+        # make data set
+        ## self.true_set : system trandition
+        ## self.obs_set : observation data
+        data = self.load_data(0)
+        self.true_set = np.array([data[0]])
+        self.obs_set = np.array([data[1]])
+
+        for i in range(1, self.file_num):
+            data = self.load_data(i)
+            true = np.array([data[0]])
+            self.true_set = np.append(self.true_set, true, axis=0)
+            obs = np.array([data[1]])
+            self.obs_set = np.append(self.obs_set, obs, axis=0)
+
+        # compute system trandition matrix/noise
+        self.matrix_set = np.zeros([self.file_num-1, self.N, self.N])
+        self.noise_set = np.zeros([self.file_num-1, self.N])
+
+        for i in range(self.file_num-1):
+            self.noise_set[i] = np.random.randn(self.N)
+            x1 = np.array([self.true_set[i+1]])
+            x0 = np.array([self.true_set[i]])
+            x0_inv = np.linalg.pinv(x0)
+            self.matrix_set[i] = np.dot(x0_inv, x1-self.noise_set[i])
+
+
+    def make_covariance(self, x0, x1):
+        x0_mean = x0.sum()/self.N
+        x1_mean = x1.sum()/self.N
+
+        P = np.zeros(self.N, self.N)
+        for i in range(self.N):
+            for j in range(self.N):
+                value = (x0[i] - x0_mean) * (x1[j] - x0_mean)
+                P[i][j] = value
+
+        return P
+
+
+    def predict(self, x, P):
+        noise = np.random.randn(self.N, self.N)
+        x_predict = np.dot(self.F_matrix[t], x) + self.noise_set[t]
+        P = self.make_covariance(x_predict, x)
+        F_trans = F.transpose()
+        P = np.dot(np.dot(F, P), F_trans) + noise
+
+        return x_predict, P
+
+
+    def update(self, x, t):
+        y = self.obs_set[t]
+        e = y - predict(x, t)[0]
+        R = self.make_covariance(x, y)
+        #S = R + self.predict(x, P)[1]
+        #K = np.dot(P, S.transpose())
+        #x = x_predict + np.dot(K, e)
+        #P_next = np.dot((np.indetity(self.N)-K), P_predict)
 
 
     def load_data(self, Nth_data):
@@ -14,22 +76,18 @@ class Kalman(object):
             print("Not Found Such Data")
             sys.exit()
 
-        with open(true_path+str(Nth_data-1)+".dat", "r") as f:
+        with open(self.true_path+str(Nth_data)+".dat", "r") as f:
             true = f.readlines()
             true = np.array([true]).astype(float)
 
-        with open(noise_path+str(Nth_data-1)+".dat", "r") as g:
+        with open(self.noise_path+str(Nth_data)+".dat", "r") as g:
             observed = g.readlines()
             observed = np.array([observed]).astype(float)
 
         return true[0], observed[0]
 
 
+
+
 if __name__ == "__main__":
-    model = Kalman(true_path=true_path, noise_path=noise_path)
-
-    a, b = model.load_data(100)
-
-    print(a)
-    print(b)
-    print(a - b)
+    model = Kalman(true_path="save_data/data", noise_path="noise_data/data")

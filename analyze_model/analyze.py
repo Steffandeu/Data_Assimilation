b0ed9e595ee91f18136dea7044e43c7a864f558f
Yusuke Kominami <komi@Yusukes-MacBook-Air.local>
2018-04-25 23:58:45 +0900

    Error Development

---
+++ analyze_model/analyze.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from lorenz96 import Lorenz96
+
+
+class Analyze(object):
+    def __init__(self, F):
+        self.F = F
+        self.N = 40
+        self.dt = 0.01
+        self.t_end = 6
+        self.x_sigma = 1e-10
+        self.noise_sigma = 1e-10
+        self.sample_num = 15
+        self.noise_sample_num = 32
+
+
+    def initialize_x(self):
+        # Initialize start point
+        init_x = np.ones([self.N]) * self.F
+        init_x += self.x_sigma * np.random.randn(self.N)
+
+        return init_x
+
+
+    def initialize_noise(self):
+        # Initialize noise
+        noise = self.noise_sigma * np.random.randn(self.N)
+
+        return noise
+
+
+    def error_develope(self):
+        # sample various error development
+        # Initialize initial x
+        init_x = self.initialize_x() + self.initialize_noise()
+        model = Lorenz96(N_dim=self.N, F=self.F, init_x=init_x)
+        true_value = model.Runge_Kutta_4(dt=self.dt, t_end=self.t_end)
+
+        # set buffer
+        data_num = int(self.t_end/self.dt)+1
+        buffer = np.zeros([data_num])
+
+        # Sample noise included version
+        for j in range(self.noise_sample_num):
+            # Initialize noise
+            x = init_x + self.initialize_noise()
+            print(init_x-x)
+            print(x)
+            print(init_x)
+            model = Lorenz96(N_dim=self.N, F=self.F, init_x=x)
+            # Check error development
+            value = model.Runge_Kutta_4(dt=self.dt, t_end=self.t_end)
+
+            # Evaluate how development
+            dev = value - true_value
+
+            # Append to buffer
+            for i in range(data_num):
+                buffer[i] = np.linalg.norm(dev[i])
+
+        return buffer
+
+
+    def show_image(self, x):
+        # plot error growth
+        t = np.arange(0, self.t_end+self.dt, self.dt)
+        plt.plot(t, x[:len(t)])
+        plt.title('F={} T={}, dt={}'.format(self.F, self.t_end, self.dt))
+        plt.tick_params(labelsize = 8)
+        #plt.xticks(np.arange(0, self.t_end+self.dt, 0.2))
+
+        plt.show()
+
+
+    def make_gif(self, x):
+        fig = plt.figure()
+        t = np.arange(0, self.t_end+self.dt, self.dt)
+
+        ims = []
+        for a in range(50):
+            y = np.sin(x - a)
+            line, = plt.plot(x, y, "r")
+            ims.append([line])
+
+        ani = animation.ArtistAnimation(fig, ims)
+        ani.save('anim.gif', writer="imagemagick")
+        ani.save('anim.mp4', writer="ffmpeg")
+        plt.show()
+
+
+if __name__ == "__main__":
+    analyzer = Analyze(F=4)
+    x = analyzer.error_develope()
+    print(x.shape)
+    analyzer.show_image(x)

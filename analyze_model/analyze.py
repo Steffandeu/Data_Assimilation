37a41327f50e6e34cec294c8cf31c52c82b12950
Yusuke Kominami <komi@Yusukes-MacBook-Air.local>
2018-04-27 14:22:09 +0900

    Enabled to plot contour

--- analyze_model/analyze.py
+++ analyze_model/analyze.py
@@ -9,11 +9,12 @@ class Analyze(object):
         self.F = F
         self.N = 40
         self.dt = 0.01
-        self.t_end = 6
+        self.t_end = 4
         self.x_sigma = 1e-10
         self.noise_sigma = 1e-10
         self.sample_num = 15
         self.noise_sample_num = 32
+        self.contour_frac = 3
 
 
     def initialize_x(self):
@@ -60,6 +61,26 @@ class Analyze(object):
         return buffer
 
 
+    def make_contour(self):
+        t_num = int(self.t_end/self.dt + self.dt)
+        t = np.arange(0, t_num, self.dt)
+        init_x = self.initialize_x()
+        model = Lorenz96(N_dim=self.N, F=self.F, init_x=init_x)
+        value = model.Runge_Kutta_4(dt=self.dt, t_end=self.t_end)
+
+        # Make hausdorff dimension by recursion system
+        # x : x[t,N]
+        x_dim = np.arange(0, self.N, float(1.0/(self.N ** (self.contour_frac-1))))
+        x_num = int(self.N ** self.contour_frac)
+        #x_buffer = np.tile(value.transpose(), x_num)
+
+        # Plot
+        plt.contour(value.transpose())
+        plt.show()
+
+
+
+
     def show_image(self, x, save=False):
         # plot error growth
         t = np.arange(0, self.t_end+self.dt, self.dt)
@@ -100,5 +121,5 @@ class Analyze(object):
 if __name__ == "__main__":
     analyzer = Analyze(F=4)
     x = analyzer.error_develope()
+    analyzer.make_contour()
     print(x.shape)
-    analyzer.show_image(x)
